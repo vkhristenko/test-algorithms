64202170593c3a3f75b87adbd89132198fa7cc29
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-27 10:29:26 +0100

    merge sort using tmp buffers for merging

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -5,54 +5,57 @@
 #include "../common/utils.hpp"
 
 void merge_impl(std::vector<int>& in, 
-                std::vector<int>& out,
                 int first, int middle, int last) {
-    int next_left = first;
-    int next_right = middle+1;
+    std::vector<int> left; left.reserve(middle-first + 1);
+    std::vector<int> right; right.reserve(last-middle);
+    int i=first;
+    while (i<=middle)
+        left.push_back(in[i++]);
+    while (i<=last)
+        right.push_back(in[i++]);
+
+    /*
+    std::cout << first << " " << middle << " " << last << "\n";
+    print_vector(std::cout, left);
+    print_vector(std::cout, right);
+    */
+
+    int next_left = 0;
+    int next_right = 0;
     int next = first;
-
-    while (next_left<=middle && next_right<=last) {
-        if (in[next_left] > in[next_right]) {
-            out[next] = in[next_right];
-            next_right++;
-        }
-        else {
-            out[next] = in[next_left];
-            next_left++;
-        }
-
-        next++;
+    while (next_left<left.size() && next_right<right.size()) {
+        if (left[next_left] > right[next_right])
+            in[next++] = right[next_right++];
+        else
+            in[next++] = left[next_left++];
     }
-    assert(next_left>middle || next_right>last);
+    assert(next_left>=left.size() || next_right>=right.size());
 
     // only one of the while loops will be actually trigger
-    while (next_left<=middle)
-        out[next++] = in[next_left++];
-    while (next_right<=last)
-        out[next++] = in[next_right++];
+    while (next_left<left.size())
+        in[next++] = left[next_left++];
+    while (next_right<right.size())
+        in[next++] = right[next_right++];
 }
 
-void merge_sort_impl(std::vector<int>& in,
-                     std::vector<int>& tmp,
+void split(std::vector<int>& in,
                      int first, int last) {
-    // if size == 1
+    // if size == 1, each guy is trivially sorted
     if (last==first) {
-        tmp[last] = in[last];
         return;
     }
 
     int middle = (last + first) / 2;
     // merge_sort left half
-    merge_sort_impl(in, tmp, first, middle);
+    split(in, first, middle);
     // merge_sort
-    merge_sort_impl(in, tmp, middle + 1, last);
+    split(in, middle + 1, last);
     // merge
-    merge_impl(tmp, in, first, middle, last);
+    merge_impl(in, first, middle, last);
 }
 
 void merge_sort(std::vector<int>& in) {
-    std::vector<int> tmp(in.size());
-    merge_sort_impl(in, tmp, 0, in.size()-1);
+    split(in, 0, in.size()-1);
 }
 
 int main(int argc, char** argv) {
