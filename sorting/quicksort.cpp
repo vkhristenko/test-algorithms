bd1d886bb9574a0db2c327ebdfa8d2157b8108b9
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-28 13:07:11 +0100

    add random pivot

--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -36,17 +36,37 @@ void quick_sort(std::vector<int>& input) {
     quick_sort_impl(input, 0, input.size()-1);
 }
 
+void rand_quick_sort_impl(std::vector<int>& input, int l, int r) {
+    if (l>=r)
+        return;
+
+    int k = (std::rand() % (r-l+1)) + l; // rand number [l, r]
+    std::swap(input[k], input[l]);
+    int m = partition(input, l, r);
+    rand_quick_sort_impl(input, l, m-1);
+    rand_quick_sort_impl(input, m+1, r);
+}
+
+void rand_quick_sort(std::vector<int>& input) {
+    rand_quick_sort_impl(input, 0, input.size() - 1);
+}
+
 int main(int argc, char **argv) {
     std::srand(std::time(nullptr));
 
     auto input = generate_random_vector<int>(std::atoi(argv[1]), 
         []{ return std::rand() % 1000; });
+    std::vector<int> input_2{input};
     print_vector(std::cout, input);
 
     quick_sort(input);
     std::cout << "***\n";
 
     print_vector(std::cout, input);
+    
+    std::cout << "***\n";
+    rand_quick_sort(input_2);
+    print_vector(std::cout, input_2);
 
     return 0;
 }
