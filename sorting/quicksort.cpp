d116dff5eb700e75d7865e5d4a8b5043a56ab7db
vkhristenko <vdkhristenko1991@gmail.com>
2019-02-28 13:45:27 +0100

    quick sort 3way needs more work

--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -22,6 +22,26 @@ int partition(std::vector<int>& input, int l, int r) {
     return j;
 }
 
+std::pair<int, int> partition_3way(std::vector<int>& input, int l, int r) {
+    int pivot = l;
+    int i = l+1, j=l;
+    int k = l;
+
+    while (i<=r) {
+        if (input[i] == input[pivot]) {
+            k++; std::swap(input[i], input[k]);
+        } else if (input[i] < input[pivot]) {
+            j++; std::swap(input[i], input[j]);
+            k++; std::swap(input[i], input[k]);
+        }
+
+        i++;
+    }
+
+    std::swap(input[pivot], input[j]);
+    return {j, k};
+}
+
 void quick_sort_impl(std::vector<int>& input, int l, int r) {
 
     if (l>=r)
@@ -51,12 +71,28 @@ void rand_quick_sort(std::vector<int>& input) {
     rand_quick_sort_impl(input, 0, input.size() - 1);
 }
 
+void rand_quick_sort_3way_impl(std::vector<int>& input, int l, int r) {
+    if (l>=r)
+        return;
+
+    int k = (std::rand() % (r-l+1)) + l; // rnd number [l, r]
+    std::swap(input[k], input[l]);
+    auto [m1, m2] = partition_3way(input, l, r);
+    rand_quick_sort_3way_impl(input, l, m1-1); // m1 was the pivot
+    rand_quick_sort_3way_impl(input, m2+1, r); // m2 was the end of equal elem reg
+}
+
+void rand_quick_sort_3way(std::vector<int>& input) {
+    rand_quick_sort_3way_impl(input, 0, input.size() - 1);
+}
+
 int main(int argc, char **argv) {
     std::srand(std::time(nullptr));
 
     auto input = generate_random_vector<int>(std::atoi(argv[1]), 
         []{ return std::rand() % 1000; });
     std::vector<int> input_2{input};
+    std::vector<int> input_3{input};
     print_vector(std::cout, input);
 
     quick_sort(input);
@@ -67,6 +103,10 @@ int main(int argc, char **argv) {
     std::cout << "***\n";
     rand_quick_sort(input_2);
     print_vector(std::cout, input_2);
+
+    std::cout << "***\n";
+    rand_quick_sort_3way(input_3);
+    print_vector(std::cout, input_3);
 
     return 0;
 }
